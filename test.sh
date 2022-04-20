9b196d39b52d65aafa760898c0cbd9fece382727
rluttrel <rluttrel@hotmail.com>
2022-04-20 18:44:11 +0000

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,10 @@
+iwctl
+station wlan0 connect ballnet
+nano /etc/pacman
+
+Color
+ParallelDownloads = 6
+ILoveCandy
+pacman -Sy pacman sshuttle
+
+sshuttle -r u0_a408@192.168.69.154:8022 0/0 -x 192.168.69.71/24
