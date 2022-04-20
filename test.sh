cd8e8a6d9aa8e997d5b85c984df267715bfb21b3
rluttrel <rluttrel@hotmail.com>
2022-04-20 19:00:36 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,6 +9,7 @@ pacman -Sy pacman sshuttle
 
 sshuttle -r u0_a408@192.168.69.154:8022 0/0 -x 192.168.69.71/24
 
-curl -Lo speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.
+curl -Lo speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
 
 chmod +x speedtest-cli
+https://github.com/rluttrel/arch2/blob/main/test.sh
